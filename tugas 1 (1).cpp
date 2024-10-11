b3ccccab4141f2173a24f9277a0273b689caeddf
FEBRIYANTI-SUNDARI <yantifebri2326@gmail.com>
2024-10-11 08:33:37 +0700

    Add files via upload

---
+++ tugas 1 (1).cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    char nim[] = "2441017"; 
+    char nama[] = "Febriyanti Sundari";
+    char jurusan[] = "Teknik Informatika"; 
+
+    printf("NIM: %s\n", nim);
+    printf("NAMA: %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+}
+
